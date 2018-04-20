97e854494fe167b9740d44e9ce8809ef331d3aa7
Christopher Pollard <crvanpollard@gmail.com>
2018-04-20 12:41:28 -0400

    added click function and image carosel
    added click function and image carosel  to GB sample

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,7 +37,7 @@
   var map = new mapboxgl.Map({
     // container id specified in the HTML
     container: 'map', 
-    style: 'mapbox://styles/mapbox/light-v9', 
+    style: 'mapbox://styles/mapbox/outdoors-v9', 
     center: [ -75.170669,39.950143], 
     bearing: 20, // Rotate Philly ~9° off of north, thanks Billy Penn.
     pitch: 50,
@@ -78,112 +78,6 @@ document.getElementById('zoomtoregion').addEventListener('click', function () {
             pitch: 35
     });
 });
-
-// curb cuts layer
-map.on('load', function () {
-
-    map.addLayer({
-        'id': 'SPG',
-        'type': 'circle',
-        'source': {
-            'type': 'geojson',
-            'data': SPG
-        },
-        'paint': {
-            'circle-radius': 4,
-            'circle-stroke-color' : '#ffffff',
-            'circle-stroke-width': 1,
-            'circle-color': 'rgba(105,136,58,.8)'
-            }
-        });
-      map.addLayer({
-        "id": "SPG-hover",
-        'type': 'circle',
-        'source': {
-            'type': 'geojson',
-            'data': SPG
-        },
-        'paint': {
-            'circle-radius': 6,
-            'circle-color': 'red'
-           
-        },
-        "filter": ["==", "name", ""]
-    });
-
-    // When the user moves their mouse over the states-fill layer, we'll update the filter in
-    // the state-fills-hover layer to only show the matching state, thus making a hover effect.
-    map.on('mousemove', 'SPG', function (e) {
-        map.setFilter('SPG-hover', ['==', 'name', e.features[0].properties.name]);
-    });
-
-    // Reset the state-fills-hover layer's filter when the mouse leaves the layer.
-    map.on('mouseleave', 'TSP', function (e) {
-        map.setFilter('TSP-hover', ['==', 'name', '']);
-    });
- // When a click event occurs on a feature in the places layer, open a popup at the
-    // location of the feature, with description HTML from its properties.
-    map.on('click', 'SPG', function (e) {
-    var features = map.queryRenderedFeatures(e.point, {
-        layers: ['SPG']
-      });
-
-      if (!features.length) {
-        return;
-      }
-
-      var feature = features[0];
-        var content = '<h4 style="color:white;background-color:rgb(105,136,58);padding: 3px;">'+ feature.properties.name+'</h4>'
-        +'<B>Project Scope:</B> '+ feature.properties.PROJSCOPE  
-        +'<br><B>Location:</B> '+ feature.properties.LOC
-        +'<br><B>State:</B> '+ feature.properties.State
-        +'<br><B>Timing:</B> '+ feature.properties.Timing
-        +'<br><table class="infotable"><thead><tr><th>Total Funded Cost<br><i>(millions in Y-O-E $)</i></th><th>Unfunded Cost<br><i>(millions in 2013 $)</i></th></tr></thead>'
-        +'<tbody><tr><td>'+ numeral(feature.properties.FundCost).format('($0,0.0)') +'</td><td> '+ numeral(feature.properties.UnfundCost).format('($0,0.0)') +'</td></tr></tbody>'
-        +'</table><br>'
-        ;
-      info.innerHTML = content;
-
-      map.flyTo({
-        center: feature.geometry.coordinates,
-        pitch: 50,
-        zoom: 14
-      }); 
-
-    });
-
-    // Change the cursor to a pointer when the mouse is over the places layer.
-    map.on('mouseenter', 'SPG', function () {
-        map.getCanvas().style.cursor = 'pointer';
-    });
-
-    // Change it back to a pointer when it leaves.
-    map.on('mouseleave', 'SPG', function () {
-        map.getCanvas().style.cursor = '';
-    });
-
-    // Create a popup, but don't add it to the map yet.
-    var popup = new mapboxgl.Popup({
-        closeButton: false,
-        closeOnClick: false
-    });
-
-    map.on('mouseenter', 'SPG', function(e) {
-        // Change the cursor style as a UI indicator.
-        map.getCanvas().style.cursor = 'pointer';
-        // Populate the popup and set its coordinates
-        // based on the feature found.
-        popup.setLngLat(e.features[0].geometry.coordinates)
-            .setHTML(e.features[0].properties.name)
-            .addTo(map);
-    });
-
-    map.on('mouseleave', 'SPG', function() {
-        map.getCanvas().style.cursor = '';
-        popup.remove();
-    });
-
-    });
 
     map.on('load', function () {
     map.addLayer({
@@ -256,39 +150,163 @@ map.on('load', function () {
     gb.className = 'marker3';
     gb.style.backgroundImage = 'assets/img/GB.png';
     gb.addEventListener('click', function() {
-        window.alert('look!!');
+      //  window.alert(marker.properties.name);
+        console.log(marker.properties);
+         var content = '<h4 style="color:white;background-color:#96c93d;padding: 3px;">'+ marker.properties.name+'</h4>'
+        +'<B>Certificate:</B> '+ marker.properties.certificat  
+        +'<br><B>Developer:</B> '+ marker.properties.developer 
+        +'<br><B>Info:</B> '+ marker.properties.uniquefeat
+        ;
+
+        if (marker.properties.photo1 === undefined){ var PHOTO1= " "  ;}
+                else { var PHOTO1 = "<div class='carousel-inner'>"+"<div class='item active'><img src='"+ (marker.properties.photo1) +"' alt='property photo'></div>"
+                +"<div class='item'><img src='"+ (marker.properties.photo2) + "' alt='property photo'></div>"
+                +"<div class='item'><img src='"+ (marker.properties.photo3) + "' alt='property photo'></div>"
+                +"<div class='item'><img src='"+ (marker.properties.photo4) + "' alt='property photo'></div></div>"
+                +" <a class='left carousel-control' href='#carousel-example-generic' data-slide='prev'>"+"<span class='glyphicon glyphicon-chevron-left'></span>"
+                +" </a>"+" <a class='right carousel-control' href='#carousel-example-generic' data-slide='next'>"+
+                "<span class='glyphicon glyphicon-chevron-right'></span>"+"</a>" ;}
+          //      if (props.Photo_Cred===undefined){ var Photo_Cred = " "  ;}
+          //      else { var Photo_Cred = "<div class='labelfieldsource'>"+ (props.Photo_Cred) +  "</div>";}
+        var  content2 = PHOTO1
+                 //   + Photo_Cred
+        document.getElementById('info').innerHTML = content;
+        document.getElementById('carousel-example-generic').innerHTML = content2;
+        $('.carousel').carousel('pause');
+
+        map.flyTo({
+        center: marker.geometry.coordinates,
+        pitch: 50,
+        zoom: 13
+        });
     });
 
     // add marker to map
     new mapboxgl.Marker(gb)
         .setLngLat(marker.geometry.coordinates)
         .addTo(map);
-});
+    });
 });
 
 map.on('load', function () {
     map.addLayer({
-        "id": "SPGicon",
+        "id": "SPG",
         "type": "symbol",
         "source": {
             "type": "geojson",
-            "data": SPGicon
+            "data": SPG
         }
     });
 
+    map.on('click', 'SPG', function (e) {
+          window.alert('look!!');
+    });
+
     // add markers to map
-    SPGicon.features.forEach(function(marker) {
+    SPG.features.forEach(function(marker) {
     // create a DOM element for the marker
     var spg = document.createElement('div');
     spg.className = 'marker4';
     spg.style.backgroundImage = 'assets/img/SPG.png';
-    spg.addEventListener('click', function() {
-        window.alert('look!!');
-    });
 
     // add marker to map
     new mapboxgl.Marker(spg)
         .setLngLat(marker.geometry.coordinates)
         .addTo(map);
+   });    
+
 });
-});
+
+// curb cuts layer
+/* map.on('load', function () {
+
+    map.addLayer({
+        'id': 'SPG',
+        'type': 'circle',
+        'source': {
+            'type': 'geojson',
+            'data': SPG
+        },
+        'paint': {
+            'circle-radius': 4,
+            'circle-stroke-color' : '#ffffff',
+            'circle-stroke-width': 1,
+            'circle-color': 'rgba(105,136,58,.8)'
+            }
+        });
+*/
+    // When the user moves their mouse over the states-fill layer, we'll update the filter in
+    // the state-fills-hover layer to only show the matching state, thus making a hover effect.
+//    map.on('mousemove', 'SPG', function (e) {
+//        map.setFilter('SPG-hover', ['==', 'name', e.features[0].properties.name]);
+//    });
+
+    // Reset the state-fills-hover layer's filter when the mouse leaves the layer.
+//    map.on('mouseleave', 'TSP', function (e) {
+//        map.setFilter('TSP-hover', ['==', 'name', '']);
+//    });
+ // When a click event occurs on a feature in the places layer, open a popup at the
+    // location of the feature, with description HTML from its properties.
+  /*  map.on('click', 'SPG', function (e) {
+    var features = map.queryRenderedFeatures(e.point, {
+        layers: ['SPG']
+      });
+
+      if (!features.length) {
+        return;
+      }
+
+      var feature = features[0];
+        var content = '<h4 style="color:white;background-color:rgb(105,136,58);padding: 3px;">'+ feature.properties.name+'</h4>'
+        +'<B>Project Scope:</B> '+ feature.properties.PROJSCOPE  
+        +'<br><B>Location:</B> '+ feature.properties.LOC
+        +'<br><B>State:</B> '+ feature.properties.State
+        +'<br><B>Timing:</B> '+ feature.properties.Timing
+        +'<br><table class="infotable"><thead><tr><th>Total Funded Cost<br><i>(millions in Y-O-E $)</i></th><th>Unfunded Cost<br><i>(millions in 2013 $)</i></th></tr></thead>'
+        +'<tbody><tr><td>'+ numeral(feature.properties.FundCost).format('($0,0.0)') +'</td><td> '+ numeral(feature.properties.UnfundCost).format('($0,0.0)') +'</td></tr></tbody>'
+        +'</table><br>'
+        ;
+      info.innerHTML = content;
+
+      map.flyTo({
+        center: feature.geometry.coordinates,
+        pitch: 50,
+        zoom: 14
+      }); 
+
+    });
+*/
+    // Change the cursor to a pointer when the mouse is over the places layer.
+ //   map.on('mouseenter', 'SPG', function () {
+ //       map.getCanvas().style.cursor = 'pointer';
+ //   });
+
+    // Change it back to a pointer when it leaves.
+ //   map.on('mouseleave', 'SPG', function () {
+ //       map.getCanvas().style.cursor = '';
+ //   });
+
+    // Create a popup, but don't add it to the map yet.
+  //  var popup = new mapboxgl.Popup({
+  //      closeButton: false,
+  //      closeOnClick: false
+  //  });
+
+ //   map.on('mouseenter', 'SPG', function(e) {
+        // Change the cursor style as a UI indicator.
+ //       map.getCanvas().style.cursor = 'pointer';
+        // Populate the popup and set its coordinates
+        // based on the feature found.
+   //     popup.setLngLat(e.features[0].geometry.coordinates)
+   //         .setHTML(e.features[0].properties.name)
+   //         .addTo(map);
+   // });
+
+  //  map.on('mouseleave', 'SPG', function() {
+  //      map.getCanvas().style.cursor = '';
+  //      popup.remove();
+  //  });
+
+  //  });
+
+
